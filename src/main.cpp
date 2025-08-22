dda891446dfb9e806392a68225f0a0f04dde543b
Loren Burkholder <loren@lorendb.dev>
2025-08-22 10:24:31 -0400

    Write log to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
+#include <QFile>
 #include <QIcon>
 #include <QQmlApplicationEngine>
+#include <QStandardPaths>
 
 #include "Aptabase.h"
 #include "Itch.h"
@@ -8,8 +10,28 @@
 
 using namespace Qt::Literals;
 
+namespace
+{
+QtMessageHandler originalHandler = nullptr;
+}
+
+// adapted from https://doc.qt.io/qt-6/qtlogging.html#qInstallMessageHandler
+void logToFile(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QString message = qFormatLogMessage(type, context, msg);
+    static QFile logFile{QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/kaon.log"_L1};
+    if (!logFile.isOpen())
+        logFile.open(QIODevice::WriteOnly);
+    logFile.write(message.toLatin1() + '\n');
+    logFile.flush();
+
+    if (originalHandler)
+        originalHandler(type, context, msg);
+}
+
 int main(int argc, char *argv[])
 {
+    originalHandler = qInstallMessageHandler(logToFile);
     qSetMessagePattern("[%{time}] [%{category}] %{type}: %{message}"_L1);
 
     QCoreApplication::setApplicationName("Kaon"_L1);
@@ -17,6 +39,8 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("LorenDB"_L1);
     QCoreApplication::setOrganizationDomain("lorendb.dev"_L1);
     QApplication app(argc, argv);
+
+    qInfo() << "This log only represents the most recent run of Kaon!";
 
     Aptabase::init("aptabase.lorendb.dev"_L1, "A-SH-5394792661"_L1);
     Aptabase::instance()->track("startup"_L1);
