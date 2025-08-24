fc077f514d04df3e84ec4feb5cea7eccd0d8f3e9
Loren Burkholder <loren@lorendb.dev>
2025-08-23 21:49:20 -0400

    Kaon 0.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     qSetMessagePattern("[%{time}] [%{category}] %{type}: %{message}"_L1);
 
     QCoreApplication::setApplicationName("Kaon"_L1);
-    QCoreApplication::setApplicationVersion("0.1.1"_L1);
+    QCoreApplication::setApplicationVersion("0.2.0"_L1);
     QCoreApplication::setOrganizationName("LorenDB"_L1);
     QCoreApplication::setOrganizationDomain("lorendb.dev"_L1);
     QApplication app(argc, argv);
